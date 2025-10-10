ade88500e7be5b48ba20c041fd330d18369737f7
Daaph <daaph.maar@gmail.com>
2025-10-09 19:29:28 -0600

    Mi primer script
    En este script se encuentran mis primeros comandos.

---
+++ Mi primer script.R
@@ -0,0 +1,22 @@
+#hola
+#aquí va el código
+#en la ventana de abajo va a salir el resultado 
+#de nuestro análisis
+
+
+# cargar matriz de datos iris en
+# el objeto BD que significa
+# Base de Datos
+BD<-iris
+
+# este comando sive para visualizar
+# una matriz de datos
+View(BD)
+
+#str visualiza y arroja el tipo de variable 
+# existen dos grandes tipos de variables, cuantitativas y cualitativas
+str(iris)
+str(BD)
+
+
+
